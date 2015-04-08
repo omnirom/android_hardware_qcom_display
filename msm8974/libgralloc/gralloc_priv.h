60a03e9c2cdc4414e2bf12d70b5a50cd3923c1cf
rooque <victor.rooque@gmail.com>
2015-04-08 17:43:27 -0300

    qcom/display: Add some flags to gralloc_priv.h
    Change-Id: I8e8f06401c2b60f92f9fd306c8dbbe5bd8dff60c

--- msm8974/libgralloc/gralloc_priv.h
+++ msm8974/libgralloc/gralloc_priv.h
@@ -54,6 +54,9 @@ enum {
      * cannot be used with noncontiguous heaps */
     GRALLOC_USAGE_PRIVATE_UNCACHED        =       0x02000000,
 
+    /* Buffer content should be displayed on an primary display only */
+    GRALLOC_USAGE_PRIVATE_INTERNAL_ONLY   =       0x04000000,
+
     /* Buffer content should be displayed on an external display only */
     GRALLOC_USAGE_PRIVATE_EXTERNAL_ONLY   =       0x08000000,
 
@@ -67,6 +70,9 @@ enum {
 
     /* CAMERA heap is a carveout heap for camera, is not secured*/
     GRALLOC_USAGE_PRIVATE_CAMERA_HEAP     =       0x00400000,
+
+     /* This flag is used for SECURE display usecase */
+    GRALLOC_USAGE_PRIVATE_SECURE_DISPLAY  =       0x00800000,
 };
 
 enum {
